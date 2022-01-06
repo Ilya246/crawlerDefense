4de72fdfb8328b29e84f3b371fbe414fc72004c0
Ilya246 <ilyukarno@gmail.com>
2022-01-06 18:16:06 +0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# crawlerDefense
+
+WIP
---
+++ src/crawlerDefense/CVars.java
@@ -0,0 +1,9 @@
+package crawler_arena;
+
+import arc.struct.*;
+import mindustry.content.UnitTypes;
+import mindustry.type.UnitType;
+
+public class CVars{
+    
+}
